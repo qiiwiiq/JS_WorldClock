43d88e56ef3756e63a5296e2f040ef3249ece7c1
PTT <qiiwiiq@hotmail.com>
2019-02-26 19:19:11 +0800

    'ver2'

--- index.js
+++ index.js
@@ -7,43 +7,28 @@ function timeDisplay() {
   let offsetTW = today.getTimezoneOffset() * 60000;
   let timestampUTC = timestampTW + offsetTW;
 
-  // 各地時差
-  let timeZone = {
-    NY: -5,
-    LON: 0,
-    BKK: 7,
-    TW: 8,
-    SYD: 10
-  };
+  // 各地時差 [NY, LON, BKK, TW, SYD]
+  let timeZone = [-5, 0, 7, 8, 10];
 
   // 取得日期時間字串，把字串變陣列
-  const getTime = offset =>
-    new Date(timestampUTC + offset * 3600000).toString().split(" ");
-
-  let timeNY = getTime(timeZone.NY);
-  let timeLON = getTime(timeZone.LON);
-  let timeBKK = getTime(timeZone.BKK);
-  let timeTW = getTime(timeZone.TW);
-  let timeSYD = getTime(timeZone.SYD);
+  const getTime = offset => new Date(timestampUTC + offset * 3600000).toString().split(" ");
+  let time = timeZone.map(getTime);
 
   // 日期標籤
   const dateText = time => `${time[2]} ${time[1].toUpperCase()}. ${time[3]}`;
+  let localDate = time.map(dateText);
 
   // 時間標籤
   const timeText = time => `${time[4].substr(0, 5)}`;
+  let localTime = time.map(timeText);
 
   // 把資料渲染到畫面
-  document.getElementById("NYdate").innerHTML = dateText(timeNY);
-  document.getElementById("LONdate").innerHTML = dateText(timeLON);
-  document.getElementById("BKKdate").innerHTML = dateText(timeBKK);
-  document.getElementById("TWdate").innerHTML = dateText(timeTW);
-  document.getElementById("SYDdate").innerHTML = dateText(timeSYD);
+  let dateDOM = document.querySelectorAll('.date');
+  let timeDOM = document.querySelectorAll('.time');
 
-  document.getElementById("NYtime").innerHTML = timeText(timeNY);
-  document.getElementById("LONtime").innerHTML = timeText(timeLON);
-  document.getElementById("BKKtime").innerHTML = timeText(timeBKK);
-  document.getElementById("TWtime").innerHTML = timeText(timeTW);
-  document.getElementById("SYDtime").innerHTML = timeText(timeSYD);
+  dateDOM.forEach((i, index) => i.innerHTML = localDate[index]);
+  timeDOM.forEach((i, index) => i.innerHTML = localTime[index]); 
+
 }
 
 timeDisplay();
